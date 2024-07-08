96eaf909ff2e715f9ae2a943261f62a334d46ec1
Scott Nesham <sanesham@gmail.com>
2024-07-08 17:50:00 -0500

    stuff

--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -1,21 +1,9 @@
 import { GraphQLSchema } from "graphql";
-import { BigIntResolver } from "graphql-scalars";
-// import { asNexusMethod, makeSchema } from "nexus";
-var { graphql, buildSchema } = require("graphql")
-// import myType from './MyType'
 import { query } from './query'
 import * as ship from './ship'
 import * as faction from './faction'
 import * as node from './node'
 
-// const BigInt = asNexusMethod(BigIntResolver, 'bigint')
-
-// export const schema = makeSchema({
-//   types: [
-//     BigInt,
-//     ...myType
-//   ]
-// })
 
 export const schema = new GraphQLSchema({
   query,
